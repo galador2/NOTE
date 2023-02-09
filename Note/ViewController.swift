d5ac65efb4f00d01e964fd997faef992bc95a491
galador2 <galador2@yandex.ru>
2023-02-09 18:11:40 +0300

    Initial Commit

---
+++ Note/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Note
+//
+//  Created by Kirill  Kostenko  on 09.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
